e2250a6588eb1fa4137017abfb9d09c6bddec81e
Roman <blackwanted@yandex.ru>
2021-06-11 12:19:02 +0300

    FF

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/usr/local/go/bin
-
 GOARCH=amd64 GOOS=linux go build  -ldflags "-s -w" ./internal/app/gam
 GOARCH=amd64 GOOS=windows go build  -ldflags "-s -w" ./internal/app/gam
 
