bd4d7bba9c0ac6f4fb6c9bbc178af03b9ba578c2
Roman <blackwanted@yandex.ru>
2021-06-11 12:18:01 +0300

    HB

--- build.sh
+++ build.sh
@@ -5,6 +5,5 @@ export PATH=$PATH:/usr/local/go/bin
 GOARCH=amd64 GOOS=linux go build  -ldflags "-s -w" ./internal/app/gam
 GOARCH=amd64 GOOS=windows go build  -ldflags "-s -w" ./internal/app/gam
 
-apt-get install zip -y
 zip -9 -r application-linux-amd64.zip gam
 zip -9 -r application-windows-amd64.zip gam.exe
